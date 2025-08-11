890fb182aa3058e770c7f5238f0868fc6e867fde
CarlosLaud <salimcarlos@gmail.com>
2025-08-11 16:54:33 -0300

    Update app3.py

--- Aula 4/app3.py
+++ Aula 4/app3.py
@@ -46,7 +46,7 @@ caminho = "https://raw.githubusercontent.com/CarlosLaud/dashboard-streamlit/refs
 df = carregar_dados(caminho)
 
 st.subheader("📊 Quantidades por Segmento")
-df_novo['Segmento'].value_counts().reset_index()
+df['Segmento'].value_counts().reset_index()
 
 # 🔄 Transformar em formato longo
 df_melted = df.melt(id_vars='Segmento', 
@@ -86,5 +86,6 @@ st.dataframe(df)
 st.subheader("💰 Valor de Mercado por Segmento")
 fig_valor, media_segmento = grafico_valor_mercado(df)
 st.pyplot(fig_valor)
+
 
 
